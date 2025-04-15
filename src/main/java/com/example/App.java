28a37ef124a47074424025cb19568967024a637b
Blazej Wcislo <67894017+blazejwcislo@users.noreply.github.com>
2025-04-15 12:57:29 +0200

    fix

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,17 @@
 package com.example;
 
-public class App {
-    public static void main(String[] args) {
+public final class App {
+
+    private App() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    /**
+     * Metoda główna.
+     *
+     * @param args argumenty wiersza poleceń
+     */
+    public static void main(final String[] args) {
         System.out.println("Hello, clean world!");
     }
 }
