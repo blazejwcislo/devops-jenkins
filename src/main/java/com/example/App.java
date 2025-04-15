0b7af8f2238d4abf53fa495907e88c52c7150d4d
Blazej Wcislo <67894017+blazejwcislo@users.noreply.github.com>
2025-04-15 13:38:19 +0200

    fix

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,11 +2,18 @@ package com.example;
 
 public final class App {
 
-private App() {
-throw new UnsupportedOperationException("Utility class");
-}
+    private App() {
+        throw new UnsupportedOperationException("Utility class");
+    }
 
+/**
+     * Main method.
+     *
+     * @param args some comment
+     *
+     * */
 public static void main(final String[] args) {
 System.out.println("Hello, clean world!");
 }
 }
+    
