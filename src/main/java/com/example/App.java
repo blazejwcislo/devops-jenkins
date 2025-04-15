4a3d07ed31bc1d164231801482ab465a461f67d4
Blazej Wcislo <67894017+blazejwcislo@users.noreply.github.com>
2025-04-15 12:29:17 +0200

    spotbugs

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        String unused = "I am never used";
+        System.out.println("Hello with bug!");
+    }
+}
