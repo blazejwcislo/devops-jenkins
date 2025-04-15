1fb1c5dce31e98aadc6954b79b880acb3e145418
Blazej Wcislo <67894017+blazejwcislo@users.noreply.github.com>
2025-04-15 14:35:57 +0200

    fix style

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,16 +3,16 @@ package com.example;
 public final class App {
 
     private App() {
-        throw new UnsupportedOperationException("Utility class");
+        throw new UnsupportedOperationException("Utility class.");
     }
 
     /**
-     * Description
+     * Description.
      *
      * @param args desc
      */
     public static void main(final String[] args) {
-        String unused = "I am never used";
+        String unused = "I am never used.";
         System.out.println("Hello, clean world!");
     }
 }
